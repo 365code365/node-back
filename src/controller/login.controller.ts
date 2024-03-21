98dfb3d497ead1323d50add2362c73b7e6e54063
io360 <614660823@qq.com>
2024-03-22 00:37:55 +0800

    add typeorm datasource

---
+++ src/controller/login.controller.ts
@@ -0,0 +1,17 @@
+import {Controller, Post} from "@midwayjs/core";
+import {Body} from "@midwayjs/core/dist/decorator/web/paramMapping";
+import {UserEntity} from "../entity/user.entity";
+
+
+@Controller('/auth')
+export class LoginController {
+
+
+
+    @Post('/login')
+    async  login(@Body() body:UserEntity){
+        console.log('body',body)
+        return body
+    }
+
+}
